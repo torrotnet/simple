29fc22a1515d5ab91c361150000c2385d20b416e
Piellia Vasyl <pellia@ukr.net>
2015-09-22 17:19:31 +0300

    add stack skills when create s story

--- success_story/views.py
+++ success_story/views.py
@@ -38,16 +38,15 @@ def success_story(request, id):
 
 def success_story_new(request):
     title = "Success story"
-    spec = [(spec.spec_title, spec.spec_title) for spec in Speciality.objects.all()]
-    stack_skills_choices = [(ss.skill, ss.skill) for ss in StackSkills.objects.all()]
+    # stack_skills_choices = [(ss.skill, ss.skill) for ss in StackSkills.objects.all()]
 
     # used_to = forms.ChoiceField(required=True, label='Project creator', choices=spec)
 
     form = SuccessStoryForm(request.POST or None)
 
-    form.fields["used_to"].choices = spec
-    form.fields["became"].choices = spec
-    form.fields["stack_skills"].choices = stack_skills_choices
+    # form.fields["used_to"].choices = spec
+    # form.fields["became"].choices = spec
+    # form.fields["stack_skills"].choices = stack_skills_choices
 
     context = {
         "title": title,
